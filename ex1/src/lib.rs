2fb77a43f5695587576f817c1bdc65b03be0283e
Tas Hasting <tas.hasting@gmail.com>
2022-02-12 21:38:34 -0600

    Exercise 1

--- ex1/src/lib.rs
+++ ex1/src/lib.rs
@@ -8,23 +8,35 @@
 //   for floating-point types, but not integers.)
 // - To rebuild and run #[test] functions, just type `cargo test`.
 
-pub fn is_prime() -> bool {
-    true;
+pub fn is_prime(n: i32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    let mut factors = 0;
+    for p in 1..n / 2 + 1 {
+        if n % p == 0 {
+            factors = factors + 1;
+        }
+    }
+    if factors < 2 {
+        return true;
+    }
+    false
 }
 
 #[test]
 fn check_primes() {
     assert!(!is_prime(0));
     assert!(!is_prime(1));
-    assert!( is_prime(2));
-    assert!( is_prime(3));
+    assert!(is_prime(2));
+    assert!(is_prime(3));
     assert!(!is_prime(4));
-    assert!( is_prime(5));
+    assert!(is_prime(5));
     assert!(!is_prime(6));
-    assert!( is_prime(7));
+    assert!(is_prime(7));
 
     assert!(!is_prime(51));
-    assert!( is_prime(53));
+    assert!(is_prime(53));
     assert!(!is_prime(1013 * 1069));
-    //assert!( is_prime(2147483647));
+    assert!(is_prime(2147483647));
 }
