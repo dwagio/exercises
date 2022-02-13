90506a6d254353457ad0d755ea4202ec254149e7
Tas Hasting <tas.hasting@gmail.com>
2022-02-12 21:46:49 -0600

    Exercise 2

--- ex2/src/lib.rs
+++ ex2/src/lib.rs
@@ -5,7 +5,7 @@
 //
 // 3) Finally, get the third test function working.
 
-pub fn count(vec: Vec<i32>, val: i32) -> usize {
+pub fn count<T: PartialEq>(vec: Vec<T>, val: T) -> usize {
     let mut count = 0;
     for elt in vec {
         if elt == val {
@@ -22,22 +22,19 @@ fn test_count_i32() {
     assert_eq!(count(vec![0, 0, 1, 5, 0], 20), 0);
 }
 
-#[cfg(skip)] // For 2), delete this line to stop skipping this test function
 #[test]
 fn test_count_other_types() {
     assert_eq!(count(vec![-128i8, 0, 127], 127), 1);
     assert_eq!(count(vec!["iichigo", "kiichigo", "momo"], "iichigo"), 1);
 }
 
-#[cfg(skip)] // Delete for 3)
 #[test]
 fn test_count_floats() {
-    use std::f64::consts::{PI, E};
+    use std::f64::consts::{E, PI};
 
     // IEEE floats are tricky: NaN != NaN, so f32 and f64 are not Eq! There is a
     // PartialEq trait, though. (Exact equality is not very meaningful on
     // floats; I know.)
     assert_eq!(count(vec![-0.25f32, 4.0, 6.022e+23, 4.0], 4.0), 2);
-    assert_eq!(count(vec![PI, E, E, PI], std::f64::consts::E),
-               2);
+    assert_eq!(count(vec![PI, E, E, PI], std::f64::consts::E), 2);
 }
